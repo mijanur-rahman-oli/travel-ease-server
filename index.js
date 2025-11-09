2ac7f43dcaee3f6c4359ffc385f35610d82379ef
mijanur-rahman-oli <sheikhmijanurrahmanoli@gmail.com>
2025-11-09 17:49:18 +0600

    vehicles collection

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const db = client.db('travelEase')
+    const vehicleCollection = db.collection('vehicles')
+
+      app.get("/vehicles", async (req, res) => {
+      const result = await vehicleCollection.find().toArray();
+      res.send(result);
+    });
+
 
 
 
